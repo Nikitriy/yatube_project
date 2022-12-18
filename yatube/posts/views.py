0fcb658e8ab0069dfd8bb80dd811d8e84a88d249
Никита Волков <iallknow1@yandex.ru>
2022-12-18 20:13:44 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+def index(request):
+    return HttpResponse('Тронный зал')
+
+
+def group_posts(request, any_slug):
+    return HttpResponse(f'Каракули отсортировано по {any_slug}')
