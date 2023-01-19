28add18b79c26a6e3dad9e7c61e7e58cd7713eda
Никита Волков <iallknow1@yandex.ru>
2023-01-19 17:04:54 +0300

    Fixed mistakes

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,9 @@ from .models import Post, Group
 # Create your views here.
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
     context = {
+        'title' : title,
         'posts' : posts,
     }
     return render(request, 'posts/index.html', context)
@@ -13,7 +15,9 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {group.title}'
     context = {
+        'title' : title,
         'group' : group,
         'posts' : posts,
     }
